e5e06485a1e79da07a0046c7d74a894ec157994a
Vineet Kumar <vineetkumar@192.168.1.7>
2026-07-31 16:22:56 +0530

    changed welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Welcome Vineet!</h1>
+          <h1>This is my first react application</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
